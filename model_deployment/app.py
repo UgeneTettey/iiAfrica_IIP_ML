37d678a1f46a5e5ddee0e6c3f1fbc4c1277a5035
Eugene <ugeneayerkain@gmail.com>
2024-08-16 00:55:53 +0000

    model file path correction

--- model_deployment/app.py
+++ model_deployment/app.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 
 # load the model
 # model = pickle.load(open('new_xgb_model.pkl', 'rb'))
-# import os
+import os
 # model_path = os.path.join(os.getcwd(), 'C:/Users/Eugene/Desktop/iiAfrica/IIP/ML/day_4/model_deployment/xgb_model.pkl')
 # model = pickle.load(open(model_path, 'rb'))
 
